b4839752c610549aa60014860eee9faf3caa6777
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-04-07 17:30:18 +0900

    station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,35 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const fullNames = userList.map(buildFullName);
+      resolve(fullNames);
+    }, 3000);
+  });
 }
 
+function buildFullName(user) {
+  const fullName = `${user.family_name} ${user.first_name}`;
+  return { ...user, full_name: fullName };
+}
